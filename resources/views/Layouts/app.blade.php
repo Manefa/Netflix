01445b82c04dc21df919440ecc41764e27a97430
Manefa <manefae8@gmail.com>
2023-11-12 19:16:09 -0500

    correction du lien derriere le logo

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -104,7 +104,7 @@
         <div class="container-lg">
 
             <!-- Logo - OPEN -->
-            <a class="navbar-brand logo" href="/netflixss">
+            <a class="navbar-brand logo" href="/netflixs">
                 <img src="{{ asset('media/images/logo.png') }}" width="80" alt="">
             </a>
             <!-- Logo - CLOSE -->
