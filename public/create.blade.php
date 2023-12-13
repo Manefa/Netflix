c3d7d4678314cd904e97c49850f208847b6b769a
Gabriel Bélair <sbrubicon12@hotmail.com>
2023-12-12 19:41:54 -0500

    Gestion erreurs

--- public/create.blade.php
+++ public/create.blade.php
@@ -3,42 +3,30 @@
 @section('title', 'Ajouter Film')
 
 @section('contenu')
-
-    {!! JsValidator::formRequest('App\Http\Requests\FilmRequest') !!}
-
     <div class="container-fluid w-50" style="margin-top: 100px;">
         <div class="row">
             <div class="col-xl-12">
                 <form method="post" action="{{ route('films.store') }}">
                     @csrf
-
+                    @if(isset($errors) && $errors->any())
+                    <div>
+                        @foreach($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                    @endif
                     <div class="form-group">
                         <label for="titreFilm">Titre du film</label>
                         <input type="text" class="form-control" id="titreFilm" placeholder="Titre" name="titre">
-                        @error('titre')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="resumeFilm">Résume du film</label>
                         <input type="text" class="form-control" id="resumeFilm" placeholder="Résume" name="resume">
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="dureeFilm">Durée du film</label>
                         <input type="text" class="form-control" id="dureeFilm" placeholder="Durée" name="duree">
-                        @error('duree')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="anneeFilm">Année de production</label>
                         <input type="text" class="form-control" id="anneeFilm" placeholder="Année"
                             name="annee_de_production">
-                        @error('annee_de_production')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="brandFilm">Brand</label>
                         <input type="text" class="form-control" id="brandFilm" placeholder="Brand" name="brand">
-                        @error('brand')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
 
                         <label for="realisateurInput">
                             Réalisateur du film
@@ -49,10 +37,7 @@
                                 <option value="{{ $personne->id }}">{{ $personne->nom }}</option>
                             @endforeach
                         </select>
-                        @error('realisateur_id')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
-
+                       
                         <label for="producteurInput">
                             Producteur du film
                             <input mbsc-input id="producteurInput" data-dropdown="true" />
@@ -62,125 +47,96 @@
                                 <option value="{{ $personne->id }}">{{ $personne->nom }}</option>
                             @endforeach
                         </select>
-                        @error('producteur_id')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="lienFilm">Lien du film</label>
                         <input type="url" class="form-control" id="lienFilm" placeholder="Lien" name="lienfilm">
-                        @error('lienfilm')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="pochetteURL">URL de la pochette</label>
                         <input type="url" class="form-control" id="pochetteURL" placeholder="Pochette"
                             name="pochette_url">
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="couvertureURL">URL de la couverture</label>
                         <input type="url" class="form-control" id="couvertureURL" placeholder="Couverture"
                             name="couverture_url">
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="coteFilm">Cote du film</label>
                         <input type="number" class="form-control" id="coteFilm" placeholder="Cote" name="cote">
-                        @error('cote')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label for="notationFilm">Notation du film</label>
                         <input type="text" class="form-control" id="notationFilm" placeholder="Notation" name="notation">
-                        @error('notation')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label>
                             Genre du film
-                            <input mbsc-input id="genreInput" data-dropdown="true" data-tags="true" />
+                            <input mbsc-input id="genreInput" data-dropdown="true" data-tags="true"/>
                         </label>
                         <select name="genre_id[]" id="multiple-select" multiple>
-                            @foreach ($genres as $genre)
-                                <option value="{{ $genre->id }}">{{ $genre->titre }}</option>
-                            @endforeach
+                        @foreach($genres as $genre)
+                            <option value="{{$genre->id}}">{{$genre->titre}}</option>
+                        @endforeach
                         </select>
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label>
                             Acteurs du film
                             <input mbsc-input id="personneInput" data-dropdown="true" data-tags="true" />
                         </label>
                         <select name="personne_id[]" id="multiple-select2" multiple>
-                            @foreach ($personnes as $personne)
-                                <option value="{{ $personne->id }}">{{ $personne->nom }}</option>
-                            @endforeach
+                        @foreach($personnes as $personne)
+                            <option value="{{$personne->id}}">{{$personne->nom}}</option>
+                        @endforeach
                         </select>
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label>
                             Langues du film
                             <input mbsc-input id="langueInput" data-dropdown="true" data-tags="true" />
                         </label>
                         <select name="langue_id[]" id="multiple-select3" multiple>
-                            @foreach ($langues as $langue)
-                                <option value="{{ $langue->id }}">{{ $langue->code }}</option>
-                            @endforeach
+                        @foreach($langues as $langue)
+                            <option value="{{$langue->id}}">{{$langue->code}}</option>
+                        @endforeach
                         </select>
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                         <label>
                             Sous titres du film
                             <input mbsc-input id="soustitreInput" data-dropdown="true" data-tags="true" />
                         </label>
                         <select name="sous_titre_id[]" id="multiple-select4" multiple>
-                            @foreach ($sous_titres as $sous_titre)
-                                <option value="{{ $sous_titre->id }}">{{ $sous_titre->code }}</option>
-                            @endforeach
+                        @foreach($sous_titres as $sous_titre)
+                            <option value="{{$sous_titre->id}}">{{$sous_titre->code}}</option>
+                        @endforeach
                         </select>
-                        @error('resume')
-                            <div style="color:red; font-weight:bold">{{ $message }}</div>
-                        @enderror
                     </div>
                     <button type="submit" class="btn btn-primary">Enregistrer</button>
                 </form>
             </div>
         </div>
     </div>
-    </body>
-    <script>
-        mobiscroll.select('#multiple-select', {
-            inputElement: document.getElementById('genreInput'),
-            touchUi: false,
-            defaultValue: '1'
-        });
+</body>
+<script>
+    mobiscroll.select('#multiple-select', {
+    inputElement: document.getElementById('genreInput'),
+    touchUi: false,
+    defaultValue: '1'
+});
 
-        mobiscroll.select('#multiple-select2', {
-            inputElement: document.getElementById('personneInput'),
-            touchUi: false,
-            defaultValue: '1'
-        });
+mobiscroll.select('#multiple-select2', {
+    inputElement: document.getElementById('personneInput'),
+    touchUi: false,
+    defaultValue: '1'
+});
 
-        mobiscroll.select('#multiple-select3', {
-            inputElement: document.getElementById('langueInput'),
-            touchUi: false,
-            defaultValue: '1'
-        });
+mobiscroll.select('#multiple-select3', {
+    inputElement: document.getElementById('langueInput'),
+    touchUi: false,
+    defaultValue: '1'
+});
 
-        mobiscroll.select('#multiple-select4', {
-            inputElement: document.getElementById('soustitreInput'),
-            touchUi: false,
-            defaultValue: '1'
-        });
+mobiscroll.select('#multiple-select4', {
+    inputElement: document.getElementById('soustitreInput'),
+    touchUi: false,
+    defaultValue: '1'
+});
 
-        mobiscroll.select('#single-select1', {
-            inputElement: document.getElementById('realisateurInput'),
-            touchUi: false,
-        });
+mobiscroll.select('#single-select1', {
+    inputElement: document.getElementById('realisateurInput'),
+    touchUi: false,
+});
 
-        mobiscroll.select('#single-select2', {
-            inputElement: document.getElementById('producteurInput'),
-            touchUi: false,
-        });
-    </script>
+mobiscroll.select('#single-select2', {
+    inputElement: document.getElementById('producteurInput'),
+    touchUi: false,
+});
+
+</script>
 
 @endsection
